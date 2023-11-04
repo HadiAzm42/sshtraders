e6c9a8abb8cefd7f0c5a3820531996288b206e0f
Hadi <hadiazeem.azeemi@gmail.com>
2023-11-04 02:25:55 -0700

    second commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "SSH Traders",
+  description: "At SSH Traders, we're dedicated to bringing you an eclectic array of products that cater to your diverse needs and interests. From cutting-edge computer accessories to exquisite jewelry, captivating books to stunning posters, we offer an extensive selection that's bound to enhance your life in various ways.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}

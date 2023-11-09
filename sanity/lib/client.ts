95852a9b1f500d5cdd1e39851d4e0d38613ab2f5
Hadi <hadiazeem.azeemi@gmail.com>
2023-11-09 15:11:26 -0800

    some changes

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "skp6Wlrv841jsTCA3nVdK90QEj5R8ZqeN2STQCW9X4oEZtX4BwFtvmVyRmwCQV0LqpJ47otnSqzJb9H8Tuz7rt77uafruLPl0ySECbUvbmujKvZHx3NjTUJQx2mJyaYdZapM3PRXsIW9iaaIhuA3GMKNzO8POp1KrIaBwny0OhIdUU6TCoXS"
 })
